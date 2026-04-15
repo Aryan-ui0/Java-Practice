0bcec2578c58b41c8757fe9db18e6ab286faf7fa
Aryan-ui0 <aryansaini7213@gmail.com>
2026-04-15 15:21:56 +0530

    collection

---
+++ collections/arraylist/one.java
@@ -0,0 +1,29 @@
+package collections.arraylist;
+
+import java.util.ArrayList;
+
+public class one {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(1);
+        al.add(2);
+        al.add(3);
+        al.add(4);
+
+        System.out.println(al);
+
+        int first = 0;
+        int last = al.size() - 1;
+
+        while (first < last) {
+            int temp = al.get(first);
+            al.set(first, al.get(last));
+            al.set(last, temp);
+
+            first++;
+            last--;
+        }
+
+        System.out.println(al);
+    }
+}
